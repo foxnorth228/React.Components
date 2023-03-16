fd5b7276b99671f2ec755c726201c5a1f0f6e195
Alex Khitry <khitrii03@gmail.com>
2023-03-16 17:30:39 +0300

    feat: add routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,35 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  Navigate,
+  RouterProvider,
+  Routes,
+} from 'react-router-dom';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route
+        path="/"
+        element={
+          <>
+            <div>Hello World</div>
+          </>
+        }
+      />
+      <Route path="/about" element={<div>About</div>} />
+      <Route path="/404" element={<div>404</div>} />
+      <Route path="*" element={<Navigate to="/404" />} />
+    </>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
