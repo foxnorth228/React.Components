afadb0c0a138fece14018642176684584f3d0fa6
Alex Khitry <khitrii03@gmail.com>
2023-03-21 09:20:54 +0300

    feat: create common view of form

--- src/Content/Page/FormPage/FormPage.tsx
+++ src/Content/Page/FormPage/FormPage.tsx
@@ -1,9 +1,42 @@
 import React from 'react';
 import './FormPage.css';
 
+// buy a program from freelance
 class FormPage extends React.Component {
   render() {
-    return <div></div>;
+    const a = new Date().toISOString().slice(0, 10);
+    return (
+      <div className="formPage">
+        <div className="form">
+          <input type="text" name="name" placeholder="Name of project..."></input>
+          <input type="number" name="cost" placeholder="How much money you offer..."></input>
+          <input type="email" name="contact" placeholder="Your contact email..."></input>
+          <input type="date" value={a} min={a}></input>
+          <select>
+            <option>FrontEnd</option>
+            <option>BackEnd</option>
+            <option>Desktop</option>
+            <option>Mobile</option>
+          </select>
+          <label>
+            Prepaymant
+            <input type="checkbox"></input>
+          </label>
+          <fieldset>
+            <label>
+              Javascript
+              <input type="radio"></input>
+            </label>
+            <label>
+              Typescript
+              <input type="radio"></input>
+            </label>
+          </fieldset>
+          <input type="file" accept="image/png, image/jpeg"></input>
+          <input type="submit" value="Send Request"></input>
+        </div>
+      </div>
+    );
   }
 }
 
