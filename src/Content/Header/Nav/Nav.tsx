83295dfb2ef74c4c0c404ac82d453b133a96cfe4
Alex Khitry <khitrii03@gmail.com>
2023-03-27 15:36:30 +0300

    feat: rewrite header

--- src/Content/Header/Nav/Nav.tsx
+++ src/Content/Header/Nav/Nav.tsx
@@ -3,18 +3,16 @@ import { Link } from 'react-router-dom';
 import './Nav.css';
 import pagesInfo from '../../../pagesInfo';
 
-class Nav extends React.Component {
-  render() {
-    return (
-      <nav className="nav">
-        {pagesInfo.map((el, i) => (
-          <Link key={i} className="header__link" to={el.link}>
-            {el.name}
-          </Link>
-        ))}
-      </nav>
-    );
-  }
+function Nav() {
+  return (
+    <nav className="nav">
+      {pagesInfo.map((el, i) => (
+        <Link key={i} className="header__link" to={el.link}>
+          {el.name}
+        </Link>
+      ))}
+    </nav>
+  );
 }
 
 export default Nav;
