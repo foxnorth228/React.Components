8dd5e239a0f5469d3bd479257e2ed8aac29ab08a
Alex Khitry <khitrii03@gmail.com>
2023-03-27 15:49:40 +0300

    feat: rewrite some files

--- src/Content/Page/MainPage/CardList/Card/LIkes/Likes.tsx
+++ src/Content/Page/MainPage/CardList/Card/LIkes/Likes.tsx
@@ -1,19 +1,13 @@
 import React from 'react';
 import './Likes.css';
 
-class Likes extends React.Component<{ likes: number }, object> {
-  constructor(props: { likes: number }) {
-    super(props);
-  }
-  render() {
-    const likes = this.props.likes;
-    return (
-      <div className="like">
-        <div className="like__icon"></div>
-        <span>{likes}</span>
-      </div>
-    );
-  }
+function Likes({ likes }: { likes: number }) {
+  return (
+    <div className="like">
+      <div className="like__icon"></div>
+      <span>{likes}</span>
+    </div>
+  );
 }
 
 export default Likes;
